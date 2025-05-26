807f2f9460f8d7b77736dc3645c11b49a4b34bc3
interesantos <clementsferdinand@gmail.com>
2025-05-26 07:02:08 -0400

    Fix i18n routing for static files

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? 'https://samuiwater.com' : '',
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+    localeDetection: false
+  },
   images: {
     domains: ['samuiwater.com'],
     remotePatterns: [
