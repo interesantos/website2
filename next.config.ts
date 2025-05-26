528e4f8d586a5111ac8e8fd1d72a3ab652d273cb
interesantos <clementsferdinand@gmail.com>
2025-05-26 06:56:49 -0400

    Fix static file routing with basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://samuiwater.com' : '',
   images: {
     domains: ['samuiwater.com'],
     remotePatterns: [
